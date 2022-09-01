1bacae7616f09ad73b96a6cf88157f6819a73ac3
ybnate254 <95563035+ybnate254@users.noreply.github.com>
2022-09-01 20:29:57 +0300

    jioni

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './nav.css'
+import {GoHome} from 'react-icons/go'
+import {FiUser} from 'react-icons/fi'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
-    <div>
-      navy
-    </div>
+    <nav>
+      <a href=".container" className={activeNav === '#' ? 'active' : ''}><GoHome/></a>
+      <a href="#about" onClick={()=> setActiveNav('#about')} ><FiUser/></a>
+      <a href="#experience" onClick={alert()}><BiBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
